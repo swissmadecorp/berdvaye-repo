393a1faa3e78ba9dc923147aee472e5c34e31b80
Edward <info@swissmadecorp.com>
2026-02-04 22:00:02 +0000

    Auto-commit 2026-02-04 22:00

--- resources/views/livewire/product-item.blade.php
+++ resources/views/livewire/product-item.blade.php
@@ -245,7 +245,6 @@
                 Slider()
             })
 
-            debugger
             $('#p_model').devbridgeAutocomplete({
                 serviceUrl: "{{route('get.retailproducts')}}",
                 showNoSuggestionNotice : true,
