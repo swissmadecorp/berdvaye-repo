eafafd7f46d13292b51efab7c9f7c60dcd46d9eb
Edward <info@swissmadecorp.com>
2026-02-06 16:10:01 +0000

    Auto-commit 2026-02-06 16:10

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -10,9 +10,9 @@ use Illuminate\Support\Facades\Mail;
 class InquiryController extends Controller
 {
    public function ajaxInquiry(Request $request) {
-    
+
        if ($request->ajax()) {
-        
+
         //    $data = array(
         //         'body'=>$request['message'],
         //         'fullname'=>$request['fullname'],
@@ -21,20 +21,45 @@ class InquiryController extends Controller
         //         'productsize' => $request['productsize'],
         //         'mobile'=> $request['mobile']
         //     );
-    
+
            $validationRules=[
                 'fullname' => 'required',
                 'email' => 'required|email',
-            ];     
+            ];
 
             $validator = \Validator::make($request->all(),$validationRules);
-            
+
+            \Validator::extend('captcha', function($attribute, $value, $parameters, $validator) use($request){
+                return captcha_check($value);
+            });
+
             if ($validator->fails()) {
-                return response()->json($validator->errors()->all());  
+                return response()->json($validator->errors()->all());
             }
-            
+
+            \Log::info('Inquiry form data: '.json_encode($request->all()));
+return;
+            $response = $inputs["g-recaptcha-response"];
+            $url = 'https://www.google.com/recaptcha/api/siteverify';
+            $data = array(
+                'secret' => config('recapcha.secret_v2'),
+                'response' => $response
+            );
+
+            $options = array(
+                'http' => array (
+                    'method' => 'POST',
+                    'content' => http_build_query($data),
+                    'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+                    "User-Agent:MyAgent/1.0\r\n",
+                )
+            );
+            $context  = stream_context_create($options);
+            $verify = file_get_contents($url, false, $context);
+            $captcha_success=json_decode($verify);
+
             //Mail::to(config('mail.from.address'))->queue(new InquiryEmail($data));
-            
+
             $data = array(
                 'to' => 'info@berdvaye.com',
                 'body'=>$request['message'],
@@ -49,12 +74,12 @@ class InquiryController extends Controller
             );
             //return response()->json($data);
             // Mail::to('info@swissmadecorp.com')->queue(new InquiryEmail($data));
-            
+
             $gmail = new Gmailer($data);
             $gmail->send();
 
            return response()->json(array('message' => 'Price request form was successfully submitted. Thank you, We will get back to you soon!'));
         }
-        
+
     }
 }
