d6c5609a56be9ec1aa388583baeb84a532d152f8
Edward <info@swissmadecorp.com>
2026-02-09 22:00:02 +0000

    Auto-commit 2026-02-09 22:00

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -38,11 +38,13 @@ class InquiryController extends Controller
             }
 
             $forbiddenWods = ['OR', 'AND', 'XOR','sleep()','sysdate()','%','concat','union','select','insert','update','delete','drop','truncate','exec','declare','--','#'];
-            foreach ($forbiddenWods as $word) {
+            return explode([' ',','], $request['message']);
+            foreach (explode(' ', $request['message']) as $word) {
                 if (in_array($word, $forbiddenWods)) {
-                        return response()->json(['message' => 'Your message contains forbidden words. Please remove them and try again.']);
+                    return response()->json(['message' => 'Your message contains forbidden words. Please remove them and try again.']);
                 }
             }
+
 
             return response()->json(['message' => 'Hello World']);
             $response = $request["g-recaptcha-response"];
