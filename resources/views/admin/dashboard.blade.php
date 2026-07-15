a6acba156fe8c4f9733588b23c47a01cef684449
Edward <info@swissmadecorp.com>
2026-07-15 15:10:01 +0000

    Auto-commit 2026-07-15 15:10

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.admin-new-default')
 
-@role('superadmin')
-
 @section ('header')
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
 @endsection
@@ -27,7 +25,7 @@
                 </tr>
             </thead>
             <tbody>
-            
+
             @foreach ($backorders as $order)
 
                 <tr class="odd:bg-white hover:bg-gray-100 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
@@ -67,7 +65,7 @@
                 </tr>
             </thead>
             <tbody>
-            
+
             @foreach ($repairs as $order)
                 <tr class="odd:bg-white hover:bg-gray-100 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                     <td class="px-3 py-2"><a href="admin/orders/{{ $order['id'] }}">{{ $order['id'] }}</a></td>
@@ -98,7 +96,7 @@
             </tr>
         </thead>
     <tbody>
-    
+
     <?php $total=0; ?>
     @foreach ($invoices as $order)
         <?php
@@ -106,7 +104,7 @@
                 $status='Unpaid';
             elseif ($order->status==1)
                 $status='Paid*';
-            elseif ($order->status==2) 
+            elseif ($order->status==2)
                 $status = "Return";
             else $status='Transferred';
         ?>
@@ -127,13 +125,13 @@
             <td class="px-3 py-2">{{ $order->b_company }}</td>
             <td class="px-3 py-2">{{ $status }}</td>
             <td class="px-3 py-2">
-                <?php 
+                <?php
                     $to = date('Y-m-d',time());
-                    
+
                     $dStart = new \DateTime($to);
                     $dEnd  = new \DateTime($order->created_at);
                     $dDiff = $dStart->diff($dEnd);
-                    
+
                 ?>
 
                 @if ($dDiff->days>365)
@@ -145,7 +143,7 @@
                 @endif
             </td>
             <?php $subtotal = $order->total - $order->payments->sum('amount') ?>
-            
+
             @foreach($order->orderReturns as $returns)
                 <?php $subtotal -= $returns->pivot->amount*$returns->pivot->qty; ?>
             @endforeach
@@ -172,9 +170,8 @@
         $("#print").click(function(e) {
             e.preventDefault();
             window.location.href="{{route('print.all.owed')}}";
-            
+
         })
     })
 </script>
     @endsection
-@endrole
--- resources/views/livewire/invoice-item.blade.php
+++ resources/views/livewire/invoice-item.blade.php
@@ -349,9 +349,10 @@
                             <button wire:click="TransferToInvoice()" type="button" class="text-white mt-4 bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Make Invoice</button>
                             @endif
 
+                            @role('superadmin|administrator')
                             <button type="button" id="send-invoice" wire:click="sendInvoice()" class="float-right text-white mt-4 bg-darkred hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Send Invoice</button>
+                            @endrole
                             <button type="button" id="print-invoice" class="float-right text-white mt-4 bg-gray-700 hover:bg-gray-800 focus:ring-4 mr-2 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Print Invoice</button>
-
                         </div>
                         @else
                         <button wire:click="saveInvoice()" type="button" class="text-white mt-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save Invoice</button>
